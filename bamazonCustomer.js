32882a5ab2d021c75adb2a712a997ffc518b1c6a
github_Hennessy666 <sonjahennessy@gmail.com>
2019-10-31 15:47:00 -0400

    bamazon.js

---
+++ bamazonCustomer.js
@@ -0,0 +1,8 @@
+//Require mysql and inquirer 
+var mysql = require('mysql');
+var inquirer= require('inquirer');
+
+// Connectingt to the bamazon_db
+var connection = mysql.createConnection({
+    
+})
