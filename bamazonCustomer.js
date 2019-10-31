cdfd54dc79c9df31b5bbde25cbf885970c25ed47
github_Hennessy666 <sonjahennessy@gmail.com>
2019-10-31 15:52:03 -0400

    connection to database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,5 +4,18 @@ var inquirer= require('inquirer');
 
 // Connectingt to the bamazon_db
 var connection = mysql.createConnection({
-    
-})
+    host: "localhost",
+    port: 3000,
+
+    //Username
+    user: "root",
+
+    //passowrd
+    password: "Keiffer_11",
+    database: "bamazon"
+});
+
+connection.connect(function(err){
+    if(err) throw err;
+    showProducts();
+});
